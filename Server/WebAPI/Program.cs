3ad070c147c810cb47fb7424c900a596a447a90d
Teo <teodora.cristiana.stoicescu@gmail.com>
2025-11-06 17:34:21 +0100

    Finished assignment 5 w/o optional files
    Took 8 minutes

--- Server/WebAPI/Program.cs
+++ Server/WebAPI/Program.cs
@@ -3,31 +3,37 @@ using RepositoryContracts;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddControllers();
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddScoped<ICommentRepository, CommentFileRepository>();
-builder.Services.AddScoped<ICommentVoteRepository, CommentVoteFileRepository>();
-builder.Services.AddScoped<IPostRepository, PostFileRepository>();
-builder.Services.AddScoped<IPostVoteRepository, PostVoteFileRepository>();
-builder.Services.AddScoped<ISubForumRepository, SubForumFileRepository>();
+// Register your repositories
 builder.Services.AddScoped<IUserRepository, UserFileRepository>();
+builder.Services.AddScoped<IPostRepository, PostFileRepository>();
+builder.Services.AddScoped<ICommentRepository, CommentFileRepository>();
+builder.Services.AddScoped<ISubForumRepository, SubForumFileRepository>();
+// Add other repositories as needed
+
+// Add CORS to allow Blazor app to connect
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowBlazor",
+        policy =>
+        {
+            policy.WithOrigins("https://localhost:7XXX") // Replace with your Blazor app port
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
-app.MapControllers();
-
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
+
+app.UseCors("AllowBlazor");
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
